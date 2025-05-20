8d0fbc4b0b15f0ecace206ec6194c9cd11025bb7
natassia-nn <natassianguyen@gmail.com>
2025-05-20 10:17:58 -0700

    added notes

--- src/jfo-section-1/HelloWorld.java
+++ src/jfo-section-1/HelloWorld.java
@@ -2,5 +2,24 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        byte x = 2;
+        short y = 1000;
+        int z = 10000;
+        long w = 1_000_000_000_000L;
+
+        int i = 0;
+        while (i < 10)
+        {
+            System.out.print(i++); // prints og value and then adds one
+        }
+
+        int j = 0;
+        System.out.println(x: "\nj loop");
+        while (j < 10)
+        {
+            System.out.println(++j); //adds one first then prints
+        }
+        
     }
 }
--- src/jfo-section-3/Chickens01.java
+++ src/jfo-section-3/Chickens01.java
@@ -1,7 +1,13 @@
 public class Chickens01 {
     public static void main(String[] args) {
         //Put yout code here
-        
+        int chickenCount;
+        int eggsPerChicken;
+        int totalEggs;
+
+            totalEggs = (chickenCount * eggsPerChicken);
+            totalEggs+= (++chickenCount);
+
         System.out.println(totalEggs);
     }   
 }
