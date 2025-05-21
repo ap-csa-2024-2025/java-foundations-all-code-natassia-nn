d045803b3a49dacf820beba441b1faff56f243b0
natassia-nn <natassianguyen@gmail.com>
2025-05-21 10:21:44 -0700

    made changes

--- src/jfo-section-3/Chickens01.java
+++ src/jfo-section-3/Chickens01.java
@@ -1,12 +1,13 @@
 public class Chickens01 {
     public static void main(String[] args) {
         //Put yout code here
-        int chickenCount;
-        int eggsPerChicken;
-        int totalEggs;
+        int chickenCount = 3;
+        int eggsPerChicken = 8;
+        int totalEggs = 0;
 
-            totalEggs = (chickenCount * eggsPerChicken);
-            totalEggs+= (++chickenCount);
+            totalEggs += (chickenCount * eggsPerChicken);
+            totalEggs += (++chickenCount) * eggsPerChicken;
+            totalEggs += (chickenCount/2) * eggsPerChicken;
 
         System.out.println(totalEggs);
     }   
